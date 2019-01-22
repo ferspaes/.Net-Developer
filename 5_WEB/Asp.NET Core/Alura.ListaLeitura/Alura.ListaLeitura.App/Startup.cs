555a3b685c5343a948a9fda985c3d7c2123db3d9
fernando.js8 <fernando.js8@outlook.com>
2019-01-22 13:42:30 -0200

    Curso Asp.NetCore, Progress 29%, OK! Success!!

--- 5_WEB/Asp.NET Core/Alura.ListaLeitura/Alura.ListaLeitura.App/Startup.cs
+++ 5_WEB/Asp.NET Core/Alura.ListaLeitura/Alura.ListaLeitura.App/Startup.cs
@@ -1,6 +1,7 @@
 ﻿using Alura.ListaLeitura.App.Repositorio;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace Alura.ListaLeitura.App
@@ -9,13 +10,45 @@ namespace Alura.ListaLeitura.App
     {
         public void Configure(IApplicationBuilder app)
         {
-            app.Run(LivrosParaLer);
+            app.Run(Rotear);
+        }
+
+        public Task Rotear(HttpContext contexto)
+        {
+            var _repo = new LivroRepositorioCSV();
+            var respostaURL = new Dictionary<string, RequestDelegate>()
+            {
+                { "/Livros/ParaLer", LivrosParaLer },
+                { "/Livros/Lendo", LivrosLendo },
+                { "/Livros/Lidos", LivrosLidos }
+            };
+
+            if (respostaURL.ContainsKey(contexto.Request.Path))
+            {
+                var metodo = respostaURL[contexto.Request.Path];
+                return metodo.Invoke(contexto);
+            }
+
+            contexto.Response.StatusCode = 404;
+            return contexto.Response.WriteAsync("Achou que escreveu a URL certo?! Achou errado colega!");
         }
 
         public Task LivrosParaLer(HttpContext contexto)
         {
             var _repo = new LivroRepositorioCSV();
             return contexto.Response.WriteAsync(_repo.ParaLer.ToString());
+        }
+
+        public Task LivrosLendo(HttpContext contexto)
+        {
+            var _repo = new LivroRepositorioCSV();
+            return contexto.Response.WriteAsync(_repo.Lendo.ToString());
+        }
+
+        public Task LivrosLidos(HttpContext contexto)
+        {
+            var _repo = new LivroRepositorioCSV();
+            return contexto.Response.WriteAsync(_repo.Lidos.ToString());
         }
     }
 }
