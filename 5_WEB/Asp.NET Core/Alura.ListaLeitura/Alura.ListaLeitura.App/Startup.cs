c6f9636e9941b9a10e203615e0d7e3018e28ea4e
fernando.js8 <fernando.js8@outlook.com>
2019-01-22 23:02:08 -0200

    Add Builder, Add Router.

--- 5_WEB/Asp.NET Core/Alura.ListaLeitura/Alura.ListaLeitura.App/Startup.cs
+++ 5_WEB/Asp.NET Core/Alura.ListaLeitura/Alura.ListaLeitura.App/Startup.cs
@@ -1,6 +1,8 @@
 ﻿using Alura.ListaLeitura.App.Repositorio;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
@@ -8,9 +10,22 @@ namespace Alura.ListaLeitura.App
 {
     public class Startup
     {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRouting();
+        }
+
         public void Configure(IApplicationBuilder app)
         {
-            app.Run(Rotear);
+            var builder = new RouteBuilder(app);
+            builder.MapRoute("Livros/ParaLer", LivrosParaLer);
+            builder.MapRoute("Livros/Lendo", LivrosLendo);
+            builder.MapRoute("Livros/Lidos", LivrosLidos);
+            var router = builder.Build();
+
+            app.UseRouter(router);
+
+            //app.Run(Rotear);
         }
 
         public Task Rotear(HttpContext contexto)
