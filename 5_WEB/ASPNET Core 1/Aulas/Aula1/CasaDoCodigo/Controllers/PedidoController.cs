c5533802af80ecc1334ec6db95715678857f3547
fernando.js8 <fernando.js8@outlook.com>
2019-02-11 07:03:50 -0200

    Progress = 5%, OK! Success!!!

--- 5_WEB/ASPNET Core 1/Aulas/Aula1/CasaDoCodigo/Controllers/PedidoController.cs
+++ 5_WEB/ASPNET Core 1/Aulas/Aula1/CasaDoCodigo/Controllers/PedidoController.cs
@@ -1,10 +1,6 @@
 ﻿using CasaDoCodigo.Models;
 using CasaDoCodigo.Repositories;
 using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace CasaDoCodigo.Controllers
 {
